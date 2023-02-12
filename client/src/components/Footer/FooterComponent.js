0ab5e492b27e320dcd23a36780fc0695e96959c4
mukul <mukul28sharma@gmail.com>
2023-02-12 13:37:40 +0530

    first release

---
+++ client/src/components/Footer/FooterComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.scss";
+
+export default function FooterComponent() {
+  return (
+    <div className="footer-component">Copyright Scribblers © 2023</div>
+  );
+}
